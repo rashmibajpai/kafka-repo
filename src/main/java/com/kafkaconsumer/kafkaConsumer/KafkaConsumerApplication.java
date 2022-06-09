e850359a0908920088222ec3eb496b98336b8c41
rashmibajpai <rashmibajpai001.mca@gmail.com>
2022-06-09 19:09:40 +0530

    added kafka consumer

---
+++ src/main/java/com/kafkaconsumer/kafkaConsumer/KafkaConsumerApplication.java
@@ -0,0 +1,47 @@
+package com.kafkaconsumer.kafkaConsumer;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class KafkaConsumerApplication {
+
+
+	List<String> messages = new ArrayList<>();
+
+	User userFromTopic = null;
+
+	@GetMapping("/consumeStringMessage")
+	public List<String> consumeMsg() {
+		return messages;
+	}
+
+	@GetMapping("/consumeJsonMessage")
+	public User consumeJsonMessage() {
+		return userFromTopic;
+	}
+
+	@KafkaListener(groupId = "rashmi-1", topics = "rashmi", containerFactory = "kafkaListenerContainerFactory")
+	public List<String> getMsgFromTopic(String data) {
+		messages.add(data);
+		return messages;
+	}
+
+	@KafkaListener(groupId = "rashmi-2", topics = "rashmi", containerFactory = "userKafkaListenerContainerFactory")
+	public User getJsonMsgFromTopic(User user) {
+		userFromTopic = user;
+		return userFromTopic;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kafkaconsumer/kafkaConsumer/KafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaconsumer.kafkaConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
